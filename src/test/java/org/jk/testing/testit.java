4205a5d972f76b3518c232913e2c419620548a92
javaking2013 <patrick.r.meadows@gmail.com>
2019-01-29 13:54:44 -0600

    Adding shutdown hook to testit class

--- src/test/java/org/jk/testing/testit.java
+++ src/test/java/org/jk/testing/testit.java
@@ -5,5 +5,10 @@ import com.jk.certmon.utility.GetCert;
 public class testit {
     public static void main(String args[]){
         System.out.println(GetCert.getRemoteCertificate("www.google.com"));
+        Runtime.getRuntime().addShutdownHook(new Thread(new Runnable(){
+            public void run(){
+                System.out.println("End of testing.");
+            }
+        }));
     }
 }
